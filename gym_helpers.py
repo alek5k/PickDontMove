1f94d0ec03c8a7f21c7971d760b86cf0b2a660d4
Aleks <aleks.krnjaic@gmail.com>
2019-10-09 12:33:57 +0200

    first commit

---
+++ gym_helpers.py
@@ -0,0 +1,19 @@
+# Takes a sample from a space (eg. action or observation space) and returns it as a flattened list
+def flatten_space_sample(space_sample):
+    import numpy as np
+    output = []
+    for entry in space_sample:
+        if isinstance(entry, np.ndarray):
+            for array_item in entry:
+                output.append(array_item)
+        else:
+            output.append(entry)
+    return output
+    # for key, value in space_sample.items():
+    #     import numpy as np
+    #     output = []
+    #     if isinstance(value, np.ndarray):
+    #         output.extend(value)
+    #     else:
+    #         output.append(entry)
+    #     return output
